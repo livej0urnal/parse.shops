a3c38e375ace1a562b001ac1a9bfeeb484d9e765
Igor Filippov <anubis3009@gmail.com>
2022-10-09 15:39:46 +0300

    #install_rbac

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,12 +20,39 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        '/css/font-face.css',
+        '/vendor/font-awesome-4.7/css/font-awesome.min.css',
+        '/vendor/font-awesome-5/css/fontawesome-all.min.css',
+        '/vendor/mdi-font/css/material-design-iconic-font.min.css',
+        '/vendor/bootstrap-4.1/bootstrap.min.css',
+        '/vendor/animsition/animsition.min.css',
+        '/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css',
+        '/vendor/wow/animate.css',
+        '/vendor/css-hamburgers/hamburgers.min.css',
+        '/vendor/slick/slick.css',
+        '/vendor/select2/select2.min.css',
+        '/vendor/perfect-scrollbar/perfect-scrollbar.css',
+        '/css/theme.css',
     ];
     public $js = [
+        '/vendor/jquery-3.2.1.min.js',
+        '/vendor/bootstrap-4.1/popper.min.js',
+        '/vendor/bootstrap-4.1/bootstrap.min.js',
+        '/vendor/slick/slick.min.js',
+        '/vendor/wow/wow.min.js',
+        '/vendor/animsition/animsition.min.js',
+        '/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js',
+        '/vendor/counter-up/jquery.waypoints.min.js',
+        '/vendor/counter-up/jquery.counterup.min.js',
+        '/vendor/circle-progress/circle-progress.min.js',
+        '/vendor/perfect-scrollbar/perfect-scrollbar.js',
+        '/vendor/chartjs/Chart.bundle.min.js',
+        '/vendor/select2/select2.min.js',
+        '/js/main.js',
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap5\BootstrapAsset'
+        //        'yii\web\YiiAsset',
+//        'yii\bootstrap\BootstrapAsset',
+        'yii\web\JqueryAsset',
     ];
 }
--- views/site/login.php
+++ views/site/login.php
@@ -31,19 +31,15 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= $form->field($model, 'password')->passwordInput() ?>
 
         <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"offset-lg-1 col-lg-3 custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+            'template' => "<div class=\"col-lg-3 custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
         ]) ?>
 
         <div class="form-group">
-            <div class="offset-lg-1 col-lg-11">
+            <div class="col-lg-11">
                 <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
             </div>
         </div>
 
     <?php ActiveForm::end(); ?>
 
-    <div class="offset-lg-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div>
 </div>
