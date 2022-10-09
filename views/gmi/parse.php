562ba1af6ee4682c78d107a24ebbb45239dd58e2
Igor Filippov <anubis3009@gmail.com>
2022-10-09 17:47:19 +0300

    #gmi_parse

--- views/gmi/parse.php
+++ views/gmi/parse.php
@@ -1,3 +1,12 @@
 <?php
 
+$gmi = 'https://gmi-trading.itemcatalog.biz/PriceCatalog/ItemsByCategory/?categoryCode=ALL_ITEMS&selectType=CATEG&page=1';
+$htmlgmi = file_get_html($gmi);
 ?>
+
+<?php  foreach ($htmlgmi->find('div.price-catalog-product-item') as $product) :?>
+    <tr>
+        <td><?= $product->title = $product->find('div.product-title' , 0)->innertext; ?></td>
+
+    </tr>
+<?php endforeach; ?>
