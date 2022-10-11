415bf2172e209f7bae0c9fff956fa57fba2360a5
Igor Filippov <anubis3009@gmail.com>
2022-10-11 17:58:19 +0300

    #save_links

---
+++ models/GmiProducts.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "gmi_products".
+ *
+ * @property int $id
+ * @property string $image
+ * @property string $title
+ * @property string $sku
+ * @property string $article
+ * @property string $units
+ * @property string $per
+ * @property string $price
+ */
+class GmiProducts extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'gmi_products';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['image', 'title', 'sku', 'article', 'units', 'per', 'price'], 'required'],
+            [['image', 'title', 'sku', 'article', 'units', 'per', 'price'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'image' => 'Image',
+            'title' => 'Title',
+            'sku' => 'Sku',
+            'article' => 'Article',
+            'units' => 'Units',
+            'per' => 'Per',
+            'price' => 'Price',
+        ];
+    }
+}
