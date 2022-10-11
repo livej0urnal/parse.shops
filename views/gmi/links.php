d192a2dd256ad3937a64235e39816537457bef4b
Igor Filippov <anubis3009@gmail.com>
2022-10-11 19:36:54 +0300

    #save_links

--- views/gmi/links.php
+++ views/gmi/links.php
@@ -4,13 +4,17 @@ for ($i = 0; $i < 20; $i = $i + 1) {
     $gmi_html = 'https://gmi-trading.itemcatalog.biz/PriceCatalog/ItemsByCategory/?categoryCode=ALL_ITEMS&selectType=CATEG&page='. $i;
     $htmlgmi = file_get_html($gmi_html);
     $articles = $htmlgmi->find('article');
+    $new_link = 0;
     if(count($articles) > 1) {
         $this_link = \app\models\Gmi::findOne(['links' => $gmi_html]);
         if(!$this_link) {
             $link = new \app\models\Gmi();
             $link->links = $gmi_html;
             $link->save();
+            $new_link ++;
         }
 
     }
 }
+
+echo 'Найдено ссылок ' . $new_link;
