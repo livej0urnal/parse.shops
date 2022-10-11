818ccca0e009d6b6be0e5ebbde4c14002f8daf09
Igor Filippov <anubis3009@gmail.com>
2022-10-11 21:42:11 +0300

    #redoctober_start

---
+++ models/Grantefoods.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "grantefoods".
+ *
+ * @property int $id
+ * @property string $links
+ */
+class Grantefoods extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'grantefoods';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['links'], 'required'],
+            [['links'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'links' => 'Links',
+        ];
+    }
+}
---
+++ models/GrantefoodsProducts.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "grantefoods_products".
+ *
+ * @property int $id
+ * @property string $image
+ * @property string $title
+ * @property string $sku
+ * @property string $article
+ * @property string $units
+ * @property string $per
+ * @property string $price
+ * @property string $updated_at
+ */
+class GrantefoodsProducts extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'grantefoods_products';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['image', 'title', 'sku', 'article', 'units', 'per', 'price', 'updated_at'], 'required'],
+            [['price'], 'string'],
+            [['image', 'title', 'sku', 'article', 'units', 'per', 'updated_at'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'image' => 'Image',
+            'title' => 'Title',
+            'sku' => 'Sku',
+            'article' => 'Article',
+            'units' => 'Units',
+            'per' => 'Per',
+            'price' => 'Price',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
