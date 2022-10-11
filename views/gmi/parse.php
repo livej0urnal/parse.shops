bcca214dfa49306b59850d1f2645d34fc7f7dca7
Igor Filippov <anubis3009@gmail.com>
2022-10-11 12:22:09 +0300

    #gmi_parse

--- views/gmi/parse.php
+++ views/gmi/parse.php
@@ -1,15 +1,26 @@
 <?php
 
-$gmi = 'https://gmi-trading.itemcatalog.biz/PriceCatalog/ItemsByCategory/?categoryCode=ALL_ITEMS&selectType=CATEG&page=13';
+$gmi = 'https://gmi-trading.itemcatalog.biz/PriceCatalog/ItemsByCategory/?categoryCode=ALL_ITEMS&selectType=CATEG&page=1';
 $htmlgmi = file_get_html($gmi);
 ?>
 
 <?php  foreach ($htmlgmi->find('article') as $product) :?>
     <tr>
+        <td><?= $product->image = $product->find('img.catalog-img ', 0)->innertext; ?></td>
+        <br>
         <td><?= $product->title = $product->find('div.product-title' , 0)->innertext; ?></td>
         <br>
         <td><?= $product->sku = $product->find('div.product-description', 0)->innertext; ?></td>
         <br>
+
+        <td><?= $product->article = $product->find('div.product-description', 1)->next_sibling('div')->innertext; ?></td>
+        <td><?= $product->units = $product->find('div.description', 0)->innertext; ?></td>
+        <br>
+        <td><?= $product->per = $product->find('div.description', 1)->innertext; ?></td>
+        <br>
+        <td><?= $product->price = $product->find('div.product-price', 0)->innertext; ?></td>
+        <br><br>
+
 
 
     </tr>
