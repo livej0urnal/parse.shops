888fe3c2cc02aa7b2467393108d4d2fe6734de20
Igor Filippov <anubis3009@gmail.com>
2022-10-11 22:04:25 +0300

    #grantefood_start

--- controllers/GrantefoodsController.php
+++ controllers/GrantefoodsController.php
@@ -60,7 +60,7 @@ class GrantefoodsController extends Controller
                     $new_product->sku = $product->sku;
                     $product->image = $product->find('img.catalog-img ', 0)->getAttribute('src');
                     $product->title = $product->find('div.product-title' , 0)->plaintext;
-                    $product->article = $product->find('div.product-description', 1)->next_sibling('div')->plaintext;
+                    $product->article = $product->find('div.product-description', 0)->next_sibling('div')->plaintext;
                     $product->units = $product->find('div.description', 0)->plaintext;
                     $product->per = $product->find('div.description', 1)->plaintext;
 
