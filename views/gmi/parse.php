f3f90530f4cc32c26b6eaaea74e5c8c0cb74e057
Igor Filippov <anubis3009@gmail.com>
2022-10-11 12:39:16 +0300

    #gmi_parse

--- views/gmi/parse.php
+++ views/gmi/parse.php
@@ -1,36 +1,42 @@
 
-
-
-
-
-
 <?php
+    $base_html = 'https://gmi-trading.itemcatalog.biz/PriceCatalog/ItemsByCategory/?categoryCode=ALL_ITEMS&selectType=CATEG&page=';
+    $i = 1;
 
-$gmi = 'https://gmi-trading.itemcatalog.biz/PriceCatalog/ItemsByCategory/?categoryCode=ALL_ITEMS&selectType=CATEG&page=1';
-$htmlgmi = file_get_html($gmi);
+    $gmi_html = $base_html . $i;
+    $htmlgmi = file_get_html($gmi_html);
     $articles = $htmlgmi->find('article');
-    echo count($articles);
+    if(count($articles) > 1) :
+
 ?>
 
-<?php  foreach ($htmlgmi->find('article') as $product) :?>
-    <tr>
-        <td><?= $product->image = $product->find('img.catalog-img ', 0)->getAttribute('src'); ?></td>
-        <img src="<?= $product->image ?>" alt="">
-        <br>
-        <td><?= $product->title = $product->find('div.product-title' , 0)->innertext; ?></td>
-        <br>
-        <td><?= $product->sku = $product->find('div.product-description', 0)->innertext; ?></td>
-        <br>
+    <?php  foreach ($htmlgmi->find('article') as $product) :?>
+    <?php $q = 1; ?>
+        <tr>
+            <td><?= $product->image = $product->find('img.catalog-img ', 0)->getAttribute('src'); ?></td>
+            <br>
+            <img src="<?= $product->image ?>" alt="">
+            <br>
+            <td><?= $product->title = $product->find('div.product-title' , 0)->innertext; ?></td>
+            <br>
+            <td><?= $product->sku = $product->find('div.product-description', 0)->innertext; ?></td>
+            <br>
 
-        <td><?= $product->article = $product->find('div.product-description', 1)->next_sibling('div')->innertext; ?></td>
-        <td><?= $product->units = $product->find('div.description', 0)->innertext; ?></td>
-        <br>
-        <td><?= $product->per = $product->find('div.description', 1)->innertext; ?></td>
-        <br>
-        <td><?= $product->price = $product->find('div.product-price', 0)->innertext; ?></td>
-        <br><br>
+            <td><?= $product->article = $product->find('div.product-description', 1)->next_sibling('div')->innertext; ?></td>
+            <td><?= $product->units = $product->find('div.description', 0)->innertext; ?></td>
+            <br>
+            <td><?= $product->per = $product->find('div.description', 1)->innertext; ?></td>
+            <br>
+            <td><?= $product->price = $product->find('div.product-price', 0)->innertext; ?></td>
+            <br><br>
 
 
 
-    </tr>
-<?php endforeach; ?>
+        </tr>
+    <?php $q++; ?>
+
+    <?php  endforeach; ?>
+        <?php if(count($articles) === $q) { $i++;} ?>
+<?php else : ?>
+    <?php exit(); ?>
+<?php endif; ?>
