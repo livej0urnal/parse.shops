04e642b4ec8cda59d8479f1b01a6b7209387d986
Igor Filippov <anubis3009@gmail.com>
2022-10-12 21:38:12 +0300

    #mobile_menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -219,9 +219,6 @@ AppAsset::register($this);
         <div class="header-mobile__bar">
             <div class="container-fluid">
                 <div class="header-mobile-inner">
-                    <a class="logo" href="index.html">
-                        <img src="/images/icon/logo-white.png" alt="CoolAdmin" />
-                    </a>
                     <button class="hamburger hamburger--slider" type="button">
                             <span class="hamburger-box">
                                 <span class="hamburger-inner"></span>
@@ -233,186 +230,59 @@ AppAsset::register($this);
         <nav class="navbar-mobile">
             <div class="container-fluid">
                 <ul class="navbar-mobile__list list-unstyled">
-                    <li class="has-sub">
-                        <a class="js-arrow" href="#">
-                            <i class="fas fa-tachometer-alt"></i>Dashboard</a>
-                        <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                            <li>
-                                <a href="index.html">Dashboard 1</a>
-                            </li>
-                            <li>
-                                <a href="index2.html">Dashboard 2</a>
-                            </li>
-                            <li>
-                                <a href="index3.html">Dashboard 3</a>
-                            </li>
-                            <li>
-                                <a href="index4.html">Dashboard 4</a>
-                            </li>
-                        </ul>
+                    <li>
+                        <a href="<?= \yii\helpers\Url::home() ?>">Dashboard</a>
                     </li>
                     <li>
-                        <a href="chart.html">
-                            <i class="fas fa-chart-bar"></i>Charts</a>
+                        <a href="<?= \yii\helpers\Url::to(['gmi/index']) ?>">GMI</a>
                     </li>
                     <li>
-                        <a href="table.html">
-                            <i class="fas fa-table"></i>Tables</a>
+                        <a href="<?= \yii\helpers\Url::to(['megafood/index']) ?>">megafood</a>
+                    </li>
+
+                    <li>
+                        <a href="<?= \yii\helpers\Url::to(['redoctober/index']) ?>">redoctober</a>
                     </li>
                     <li>
-                        <a href="form.html">
-                            <i class="far fa-check-square"></i>Forms</a>
+                        <a href="<?= \yii\helpers\Url::to(['grantefoods/index']) ?>">grantefoods</a>
+                    </li>
+
+                    <li>
+                        <a href="<?= \yii\helpers\Url::to(['three/index']) ?>">three</a>
+                    </li>
+
+                    <li>
+                        <a href="<?= \yii\helpers\Url::to(['tamani/index']) ?>">tamani</a>
+                    </li>
+
+                    <li>
+                        <a href="<?= \yii\helpers\Url::to(['mamta/index']) ?>">mamta</a>
+                    </li>
+
+                    <li>
+                        <a href="<?= \yii\helpers\Url::to(['alex/index']) ?>">alex</a>
                     </li>
                     <li>
-                        <a href="calendar.html">
-                            <i class="fas fa-calendar-alt"></i>Calendar</a>
+                        <a href="<?= \yii\helpers\Url::to(['baltic/index']) ?>">baltic</a>
                     </li>
+
                     <li>
-                        <a href="map.html">
-                            <i class="fas fa-map-marker-alt"></i>Maps</a>
+                        <a href="<?= \yii\helpers\Url::to(['zakuson/index']) ?>">zakuson</a>
                     </li>
-                    <li class="has-sub">
-                        <a class="js-arrow" href="#">
-                            <i class="fas fa-copy"></i>Pages</a>
-                        <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                            <li>
-                                <a href="login.html">Login</a>
-                            </li>
-                            <li>
-                                <a href="register.html">Register</a>
-                            </li>
-                            <li>
-                                <a href="forget-pass.html">Forget Password</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="has-sub">
-                        <a class="js-arrow" href="#">
-                            <i class="fas fa-desktop"></i>UI Elements</a>
-                        <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                            <li>
-                                <a href="button.html">Button</a>
-                            </li>
-                            <li>
-                                <a href="badge.html">Badges</a>
-                            </li>
-                            <li>
-                                <a href="tab.html">Tabs</a>
-                            </li>
-                            <li>
-                                <a href="card.html">Cards</a>
-                            </li>
-                            <li>
-                                <a href="alert.html">Alerts</a>
-                            </li>
-                            <li>
-                                <a href="progress-bar.html">Progress Bars</a>
-                            </li>
-                            <li>
-                                <a href="modal.html">Modals</a>
-                            </li>
-                            <li>
-                                <a href="switch.html">Switchs</a>
-                            </li>
-                            <li>
-                                <a href="grid.html">Grids</a>
-                            </li>
-                            <li>
-                                <a href="fontawesome.html">Fontawesome Icon</a>
-                            </li>
-                            <li>
-                                <a href="typo.html">Typography</a>
-                            </li>
-                        </ul>
-                    </li>
+
                 </ul>
             </div>
         </nav>
     </header>
     <div class="sub-header-mobile-2 d-block d-lg-none">
         <div class="header__tool">
-            <div class="header-button-item has-noti js-item-menu">
-                <i class="zmdi zmdi-notifications"></i>
-                <div class="notifi-dropdown notifi-dropdown--no-bor js-dropdown">
-                    <div class="notifi__title">
-                        <p>You have 3 Notifications</p>
-                    </div>
-                    <div class="notifi__item">
-                        <div class="bg-c1 img-cir img-40">
-                            <i class="zmdi zmdi-email-open"></i>
-                        </div>
-                        <div class="content">
-                            <p>You got a email notification</p>
-                            <span class="date">April 12, 2018 06:50</span>
-                        </div>
-                    </div>
-                    <div class="notifi__item">
-                        <div class="bg-c2 img-cir img-40">
-                            <i class="zmdi zmdi-account-box"></i>
-                        </div>
-                        <div class="content">
-                            <p>Your account has been blocked</p>
-                            <span class="date">April 12, 2018 06:50</span>
-                        </div>
-                    </div>
-                    <div class="notifi__item">
-                        <div class="bg-c3 img-cir img-40">
-                            <i class="zmdi zmdi-file-text"></i>
-                        </div>
-                        <div class="content">
-                            <p>You got a new file</p>
-                            <span class="date">April 12, 2018 06:50</span>
-                        </div>
-                    </div>
-                    <div class="notifi__footer">
-                        <a href="#">All notifications</a>
-                    </div>
-                </div>
-            </div>
-            <div class="header-button-item js-item-menu">
-                <i class="zmdi zmdi-settings"></i>
-                <div class="setting-dropdown js-dropdown">
-                    <div class="account-dropdown__body">
-                        <div class="account-dropdown__item">
-                            <a href="#">
-                                <i class="zmdi zmdi-account"></i>Account</a>
-                        </div>
-                        <div class="account-dropdown__item">
-                            <a href="#">
-                                <i class="zmdi zmdi-settings"></i>Setting</a>
-                        </div>
-                        <div class="account-dropdown__item">
-                            <a href="#">
-                                <i class="zmdi zmdi-money-box"></i>Billing</a>
-                        </div>
-                    </div>
-                    <div class="account-dropdown__body">
-                        <div class="account-dropdown__item">
-                            <a href="#">
-                                <i class="zmdi zmdi-globe"></i>Language</a>
-                        </div>
-                        <div class="account-dropdown__item">
-                            <a href="#">
-                                <i class="zmdi zmdi-pin"></i>Location</a>
-                        </div>
-                        <div class="account-dropdown__item">
-                            <a href="#">
-                                <i class="zmdi zmdi-email"></i>Email</a>
-                        </div>
-                        <div class="account-dropdown__item">
-                            <a href="#">
-                                <i class="zmdi zmdi-notifications"></i>Notifications</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="account-wrap">
                 <div class="account-item account-item--style2 clearfix js-item-menu">
                     <div class="image">
                         <img src="/images/icon/avatar-01.jpg" alt="John Doe" />
                     </div>
                     <div class="content">
-                        <a class="js-acc-btn" href="#">john doe</a>
+                        <a class="js-acc-btn" href="#"><?= Yii::$app->user->identity->username ?></a>
                     </div>
                     <div class="account-dropdown js-dropdown">
                         <div class="info clearfix">
@@ -422,28 +292,17 @@ AppAsset::register($this);
                                 </a>
                             </div>
                             <div class="content">
-                                <h5 class="name">
-                                    <a href="#">john doe</a>
-                                </h5>
-                                <span class="email">johndoe@example.com</span>
+                                <span class="email"><?= Yii::$app->user->identity->username ?></span>
                             </div>
                         </div>
                         <div class="account-dropdown__body">
                             <div class="account-dropdown__item">
-                                <a href="#">
-                                    <i class="zmdi zmdi-account"></i>Account</a>
-                            </div>
-                            <div class="account-dropdown__item">
-                                <a href="#">
-                                    <i class="zmdi zmdi-settings"></i>Setting</a>
-                            </div>
-                            <div class="account-dropdown__item">
-                                <a href="#">
-                                    <i class="zmdi zmdi-money-box"></i>Billing</a>
+                                <a href="/admin">
+                                    <i class="zmdi zmdi-account"></i>Accounts</a>
                             </div>
                         </div>
                         <div class="account-dropdown__footer">
-                            <a href="#">
+                            <a href="/site/logout">
                                 <i class="zmdi zmdi-power"></i>Logout</a>
                         </div>
                     </div>
