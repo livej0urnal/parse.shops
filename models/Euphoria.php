05572d13cde301a4ba50f0e9a768014c1c464c38
Igor Filippov <anubis3009@gmail.com>
2022-10-13 19:49:26 +0300

    #euphoria_start

---
+++ models/Euphoria.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "euphoria".
+ *
+ * @property int $id
+ * @property string $links
+ */
+class Euphoria extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'euphoria';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['links'], 'required'],
+            [['links'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'links' => 'Links',
+        ];
+    }
+}
