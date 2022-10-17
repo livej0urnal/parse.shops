8588a8ed2fea8bcbcc22a40c8c15a96930691425
Igor Filippov <anubis3009@gmail.com>
2022-10-17 23:09:43 +0300

    #out_stock

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,6 +2,25 @@
 
 namespace app\controllers;
 
+use app\models\AlexmeatProducts;
+use app\models\BalticProducts;
+use app\models\EicProducts;
+use app\models\EuphoriaProducts;
+use app\models\GmiProducts;
+use app\models\GrantefoodsProducts;
+use app\models\LeaderProducts;
+use app\models\LeaProducts;
+use app\models\MamtaProducts;
+use app\models\MegafoodProducts;
+use app\models\PsvProducts;
+use app\models\RedoctoberProducts;
+use app\models\RoyalProducts;
+use app\models\SakhalinProducts;
+use app\models\StradivaProducts;
+use app\models\TamaniProducts;
+use app\models\ThreeProducts;
+use app\models\ZakusonProducts;
+use app\models\ZenithProducts;
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
@@ -9,8 +28,9 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\controllers\AppController;
 
-class SiteController extends Controller
+class SiteController extends AppController
 {
     /**
      * {@inheritdoc}
@@ -61,7 +81,36 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $id = Yii::$app->request->get('id');
+        $products = [];
+        $products_alex = AlexmeatProducts::find()->all();
+        $products_baltic = BalticProducts::find()->all();
+        $products_eic = EicProducts::find()->all();
+        $products_euphoria = EuphoriaProducts::find()->all();
+        $products_gmi = GmiProducts::find()->all();
+        $products_grantefoods = GrantefoodsProducts::find()->all();
+        $products_lea = LeaProducts::find()->all();
+        $products_leader = LeaderProducts::find()->all();
+        $products_mamta = MamtaProducts::find()->all();
+        $products_megafood = MegafoodProducts::find()->all();
+        $products_natars = MegafoodProducts::find()->all();
+        $products_psv = PsvProducts::find()->all();
+        $products_redoctober = RedoctoberProducts::find()->all();
+        $products_royal = RoyalProducts::find()->all();
+        $products_sakhalin = SakhalinProducts::find()->all();
+        $products_stradiva = StradivaProducts::find()->all();
+        $products_tamani = TamaniProducts::find()->all();
+        $products_three = ThreeProducts::find()->all();
+        $products_zakuson = ZakusonProducts::find()->all();
+        $products_zenith = ZenithProducts::find()->all();
+
+        $products = count($products_baltic) + count($products_alex) + count($products_eic) + count($products_euphoria) + count($products_gmi) + count($products_grantefoods)
+            + count($products_lea) + count($products_leader) + count($products_mamta) + count($products_megafood) + count($products_natars) + count($products_psv)
+            + count($products_redoctober) + count($products_royal) + count($products_sakhalin) + count($products_stradiva) + count($products_tamani) + count($products_three)
+            + count($products_zakuson) + count($products_zenith);
+
+        $this->setMeta('Dashboard Panel');
+        return $this->render('index', compact('products'));
     }
 
     /**
--- views/site/index.php
+++ views/site/index.php
@@ -1,4 +1,31 @@
-
+<section class="au-breadcrumb2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="au-breadcrumb-content">
+                    <div class="au-breadcrumb-left">
+                        <span class="au-breadcrumb-span">You are here:</span>
+                        <ul class="list-unstyled list-inline au-breadcrumb__list">
+                            <li class="list-inline-item active">
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="list-inline-item seprate">
+                                <span>/</span>
+                            </li>
+                            <li class="list-inline-item">Dashboard</li>
+                        </ul>
+                    </div>
+                    <form class="au-form-icon--sm" action="" method="post">
+                        <input class="au-input--w300 au-input--style2" type="text" placeholder="Search everything...">
+                        <button class="au-btn--submit2" type="submit">
+                            <i class="zmdi zmdi-search"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <!-- WELCOME-->
 <section class="welcome p-t-10">
     <div class="container">
@@ -13,7 +40,48 @@
     </div>
 </section>
 <!-- END WELCOME-->
-
+<section class="statistic statistic2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-3">
+                <div class="statistic__item statistic__item--green">
+                    <h2 class="number"> <?php echo $products; ?></h2>
+                    <span class="desc">Products </span>
+                    <div class="icon">
+                        <i class="zmdi zmdi-account-o"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="statistic__item statistic__item--orange">
+                    <h2 class="number">388,688</h2>
+                    <span class="desc">items sold</span>
+                    <div class="icon">
+                        <i class="zmdi zmdi-shopping-cart"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="statistic__item statistic__item--blue">
+                    <h2 class="number">1,086</h2>
+                    <span class="desc">this week</span>
+                    <div class="icon">
+                        <i class="zmdi zmdi-calendar-note"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="statistic__item statistic__item--red">
+                    <h2 class="number">$1,060,386</h2>
+                    <span class="desc">total earnings</span>
+                    <div class="icon">
+                        <i class="zmdi zmdi-money"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <div class="h-100" style="min-height: 100px;">
 
 </div>
