115a476f3d38615581dd3397fbab261fd8c035ff
Igor Filippov <anubis3009@gmail.com>
2022-10-17 20:36:07 +0300

    #out_stock

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -293,11 +293,31 @@ AppAsset::register($this);
     <div class="page-content--bgf7">
         <?= $content ?>
     </div>
+    <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
 
 </div>
+<script>
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+</script>
 
 <?php $this->endBody() ?>
 </body>
+
 
 </html>
 <!-- end document-->
