d9acf0206875ee75a62a1c1ff8dcd7c6baa4674c
Igor Filippov <anubis3009@gmail.com>
2022-10-17 20:10:57 +0300

    #out_stock

--- models/MamtaProducts.php
+++ models/MamtaProducts.php
@@ -38,9 +38,11 @@ class MamtaProducts extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['image', 'title', 'sku', 'article', 'units', 'per', 'price', 'updated_at'], 'required'],
-            [['price'], 'string'],
+            [['image', 'title', 'sku', 'article', 'units', 'per', 'price', 'updated_at', 'instock'], 'required'],
             [['image', 'title', 'sku', 'article', 'units', 'per', 'updated_at'], 'string', 'max' => 255],
+            [['price'], 'trim'],
+            [['instock'], 'integer'],
+            [['sku'] , 'unique' , 'comboNotUnique' => 'Username already taken!',  'targetClass' => 'app\models\GmiProducts'],
         ];
     }
 
@@ -59,6 +61,7 @@ class MamtaProducts extends \yii\db\ActiveRecord
             'per' => 'Per',
             'price' => 'Price',
             'updated_at' => 'Updated At',
+            'instock' => 'In Stock'
         ];
     }
 }
