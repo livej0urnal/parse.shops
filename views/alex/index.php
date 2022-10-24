8e54882e20fcb1c56eecb73fdc6b014e8789a4c1
Igor Filippov <anubis3009@gmail.com>
2022-10-24 21:07:26 +0300

    #change_part#1

--- views/alex/index.php
+++ views/alex/index.php
@@ -87,7 +87,7 @@ use dosamigos\chartjs\ChartJs;
                                     <td><?= $product->article ?></td>
                                     <td><?= $product->units ?></td>
                                     <td><?= $product->per ?></td>
-                                    <td>$<?= $product->price ?> <?php if(!empty($last_update)) :  ?><br><span class="mark title--sbold" style="color: red"><?php $result = $product->price - $last_update->price; if($result > 0) {echo '+';} else {echo '-';} echo $result;  ?></span><?php endif; ?></td>
+                                    <td>$<?= $product->price ?> <?php if(!empty($last_update)) :  ?><br><span class="mark title--sbold" style="color: red"><?php echo $product->price - $last_update->price;  ?></span><?php endif; ?></td>
                                     <td><?php if($product->instock === null) : ?> <span style="color:red;">out</span> <?php else : ?> <span style="color:green;">in</span> <?php endif; ?></td>
                                     <td><?= $product->seller ?></td>
                                 </tr>
