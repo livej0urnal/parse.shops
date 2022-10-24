9267daf31fde75b949891f0bd61f9dc6afaf32ce
Igor Filippov <anubis3009@gmail.com>
2022-10-24 20:05:03 +0300

    #change_part#1

--- controllers/AlexController.php
+++ controllers/AlexController.php
@@ -19,7 +19,7 @@ class AlexController extends AppController
         return [
             [
                 'class' => 'yii\filters\PageCache',
-                'only' => [''],
+                'only' => ['index'],
                 'duration' => 3600,
                 'variations' => [
                     Yii::$app->language,
