4832b84078977d4bb0b214c4e08ad22086642ef3
Igor Filippov <anubis3009@gmail.com>
2022-10-27 19:23:54 +0300

    #last-updates

--- views/site/last.php
+++ views/site/last.php
@@ -1,6 +1,7 @@
 <?php
     use yii\helpers\Url;
     use yii\helpers\Html;
+    use dosamigos\chartjs\ChartJs;
 ?>
 
 <section class="au-breadcrumb2">
@@ -52,4 +53,120 @@
     </div>
 </section>
 
-<?php debug($products_all); ?>
+<section class="p-t-20">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="table-responsive table-responsive-data2">
+
+                    <table class="table table-data2">
+                        <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Title</th>
+                            <th>SKU</th>
+                            <th>Manufacture</th>
+                            <th>Units</th>
+                            <th>Per</th>
+                            <th>Price</th>
+                            <th>In stock</th>
+                            <th>Seller</th>
+                        </tr>
+                        </thead>
+                        <?php if (!empty($products_all)) : ?>
+                            <tbody>
+                            <?php foreach ($products_all as $product) : ?>
+                                <?php $updates = $product->updates; ?>
+                                <?php
+                                $last_update = null;
+                                foreach ($updates as $single) {
+                                    if ($single->price != $product->price) {
+                                        $last_update = $single;
+                                    }
+                                }
+                                ?>
+                                <tr class="tr-shadow find-gmi-updates <?php if (!empty($last_update)) : ?>mark<?php endif; ?>"
+                                    data-value="<?= $product->sku ?>">
+                                    <td><img loading="lazy" class="img-product" src="<?= $product->image ?>" alt=""
+                                             width="300" height="300"></td>
+                                    <td><?= $product->title ?></td>
+                                    <td><?= $product->sku ?></td>
+                                    <td><?= $product->article ?></td>
+                                    <td><?= $product->units ?></td>
+                                    <td><?= $product->per ?></td>
+                                    <td>$<?= $product->price ?> <?php if (!empty($last_update)) : ?><br><span
+                                                class="mark title--sbold"
+                                                style="color: red"><?php echo round($product->price - $last_update->price, 2); ?></span><?php endif; ?>
+                                    </td>
+                                    <td><?php if ($product->instock === null) : ?> <span
+                                                style="color:red;">out</span> <?php else : ?> <span
+                                                style="color:green;">in</span> <?php endif; ?></td>
+                                    <td><?= $product->seller ?></td>
+                                </tr>
+
+                                <?php $updates = $product->updates; ?>
+                                <?php if (count($updates) > 1) : ?>
+                                    <?php
+                                    foreach ($updates as $item) {
+                                        $dates[] = Yii::$app->formatter->asDate($item['update_at'], 'php:m-d');
+                                        $prices[] = $item['price'];
+                                    }
+
+                                    ?>
+                                    <tr class="spacer tr-shadow-hidden disabled disabled-<?= $product->sku ?>">
+                                        <td colspan="9">
+                                            <?= ChartJs::widget([
+                                                'type' => 'line',
+                                                'data' => [
+                                                    'labels' => $dates,
+                                                    'datasets' => [
+                                                        [
+                                                            'backgroundColor' => "rgba(179,181,198,0.2)",
+                                                            'borderColor' => "rgba(179,181,198,1)",
+                                                            'pointBackgroundColor' => "rgba(179,181,198,1)",
+                                                            'pointBorderColor' => "#fff",
+                                                            'pointHoverBackgroundColor' => "#fff",
+                                                            'pointHoverBorderColor' => "rgba(179,181,198,1)",
+                                                            'data' => $prices,
+                                                            'fill' => false,
+                                                            'stepped' => true
+                                                        ],
+
+                                                    ]
+                                                ]
+                                            ]);
+                                            ?>
+                                        </td>
+
+                                    </tr>
+                                    <tr class="spacer"></tr>
+                                    <?php $dates = [];
+                                    $prices = []; ?>
+                                <?php endif; ?>
+                                <tr class="spacer"></tr>
+
+                            <?php endforeach; ?>
+                            </tbody>
+                        <?php else: ?>
+                            <h4>Empty</h4>
+                        <?php endif; ?>
+                    </table>
+                </div>
+                <?php if (!empty($pages)) : ?>
+                    <div class="demo-inline-spacing">
+                        <!-- Basic Pagination -->
+                        <nav aria-label="Page navigation" class="item-pagination">
+                            <?php echo LinkPager::widget([
+                                'pagination' => $pages,
+                                'options' => ['class' => 'pagination tab-paginations'],
+                                'linkOptions' => ['class' => 'page-link'],
+                            ]); ?>
+                        </nav>
+                        <!--/ Basic Pagination -->
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</section>
