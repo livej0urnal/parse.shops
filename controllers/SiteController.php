5d5aaecc5e346bf6f36a582c3d3f000e0a4c6ba5
Igor Filippov <anubis3009@gmail.com>
2022-10-27 21:22:29 +0300

    #cache_of_last

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -58,6 +58,11 @@ class SiteController extends AppController
                     'logout' => ['post', 'get'],
                 ],
             ],
+            'cache' => [
+                'class' => 'yii\filters\PageCache',
+                'only' => ['last'],
+                'duration' => 3600,
+            ],
         ];
     }
 
@@ -111,6 +116,7 @@ class SiteController extends AppController
         $products_zenith = ZenithProducts::find()->orderBy(['updated_at' => SORT_DESC])->limit(5)->all();
         $products_all = array_merge($products_baltic, $products_alex, $products_eic, $products_euphoria, $products_gmi, $products_grantefoods, $products_lea, $products_leader, $products_mamta, $products_megafood,
             $products_natars, $products_psv, $products_redoctober, $products_royal, $products_sakhalin, $products_stradiva, $products_tamani, $products_three, $products_zakuson, $products_zenith);
+
         $this->setMeta('Dashboard Panel');
         return $this->render('index', compact('products', 'out_stock', 'today', 'users', 'products_alex', 'products_all'));
     }
@@ -246,36 +252,36 @@ class SiteController extends AppController
     public function actionLast()
     {
         $id = Yii::$app->request->get('id');
-//        $products_alex = AlexmeatProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        $products_alex = AlexmeatProducts::getDb()->cache(function ($db)
-        {
-            return  $products_alex = AlexmeatProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        });
-//        $products_baltic = BalticProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        $products_baltic = BalticProducts::getDb()->cache(function ($db)
-        {
-            return $products_baltic = BalticProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        });
-//        $products_eic = EicProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        $products_eic = EicProducts::getDb()->cache(function ($db)
-        {
-            return $products_eic = EicProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        });
-//        $products_euphoria = EuphoriaProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        $products_euphoria = EuphoriaProducts::getDb()->cache(function ($db)
-        {
-            return  $products_euphoria = EuphoriaProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        });
-//        $products_gmi = GmiProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        $products_gmi = GmiProducts::getDb()->cache(function ($db)
-        {
-            return  $products_gmi = GmiProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        });
-//        $products_grantefoods = GrantefoodsProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        $products_grantefoods = GrantefoodsProducts::getDb()->cache(function ($db)
-        {
-            return $products_grantefoods = GrantefoodsProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
-        });
+        $products_alex = AlexmeatProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        $products_alex = AlexmeatProducts::getDb()->cache(function ($db)
+//        {
+//            return  $products_alex = AlexmeatProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        });
+        $products_baltic = BalticProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        $products_baltic = BalticProducts::getDb()->cache(function ($db)
+//        {
+//            return $products_baltic = BalticProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        });
+        $products_eic = EicProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        $products_eic = EicProducts::getDb()->cache(function ($db)
+//        {
+//            return $products_eic = EicProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        });
+        $products_euphoria = EuphoriaProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        $products_euphoria = EuphoriaProducts::getDb()->cache(function ($db)
+//        {
+//            return  $products_euphoria = EuphoriaProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        });
+        $products_gmi = GmiProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        $products_gmi = GmiProducts::getDb()->cache(function ($db)
+//        {
+//            return  $products_gmi = GmiProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        });
+        $products_grantefoods = GrantefoodsProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        $products_grantefoods = GrantefoodsProducts::getDb()->cache(function ($db)
+//        {
+//            return $products_grantefoods = GrantefoodsProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+//        });
         $products_lea = LeaProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
         $products_leader = LeaderProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
         $products_mamta = MamtaProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
@@ -290,17 +296,18 @@ class SiteController extends AppController
         $products_three = ThreeProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
         $products_zakuson = ZakusonProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
         $products_zenith = ZenithProducts::find()->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+        $products_all = array_merge($products_baltic, $products_alex, $products_eic, $products_euphoria, $products_gmi, $products_grantefoods, $products_lea, $products_leader, $products_mamta, $products_megafood,
+            $products_natars, $products_psv, $products_redoctober, $products_royal, $products_sakhalin, $products_stradiva, $products_tamani, $products_three, $products_zakuson, $products_zenith);
 
-
-        $products_all= Yii::$app->cache->get('products_all');
-        if($products_all===false)
-        {
-            // обновляем $value, т.к. переменная не найдена в кэше,
-            $products_all = array_merge($products_baltic, $products_alex, $products_eic, $products_euphoria, $products_gmi, $products_grantefoods, $products_lea, $products_leader, $products_mamta, $products_megafood,
-                $products_natars, $products_psv, $products_redoctober, $products_royal, $products_sakhalin, $products_stradiva, $products_tamani, $products_three, $products_zakuson, $products_zenith);
-            // и сохраняем в кэш для дальнейшего использования:
-             Yii::$app->cache->set('products_all',$products_all);
-        }
+//        $products_all= Yii::$app->cache->get('products_all');
+//        if($products_all===false)
+//        {
+//            // обновляем $value, т.к. переменная не найдена в кэше,
+//            $products_all = array_merge($products_baltic, $products_alex, $products_eic, $products_euphoria, $products_gmi, $products_grantefoods, $products_lea, $products_leader, $products_mamta, $products_megafood,
+//                $products_natars, $products_psv, $products_redoctober, $products_royal, $products_sakhalin, $products_stradiva, $products_tamani, $products_three, $products_zakuson, $products_zenith);
+//            // и сохраняем в кэш для дальнейшего использования:
+//             Yii::$app->cache->set('products_all',$products_all);
+//        }
         $this->setMeta('Last Updates from 7 days');
         return $this->render('last', compact('products_all'));
     }
