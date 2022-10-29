bbba818f8725feb19dea63a5c4c514bcf37d430b
Igor Filippov <anubis3009@gmail.com>
2022-10-29 22:55:55 +0300

    #refactoring_updates

--- views/updates/index.php
+++ views/updates/index.php
@@ -113,49 +113,6 @@
                                     <td><?= $product->seller ?></td>
                                 </tr>
 
-
-                                <?php if(!empty($last_update)) : ?>
-                                    <?php $updates = $product->updates; ?>
-                                    <?php if (count($updates) > 1) : ?>
-                                        <?php
-                                        foreach ($updates as $item) {
-                                            $dates[] = Yii::$app->formatter->asDate($item['update_at'], 'php:m-d');
-                                            $prices[] = $item['price'];
-                                        }
-
-                                        ?>
-                                        <tr class="spacer tr-shadow-hidden disabled disabled-<?= $product->sku ?>">
-                                            <td colspan="9">
-                                                <?= ChartJs::widget([
-                                                    'type' => 'line',
-                                                    'data' => [
-                                                        'labels' => $dates,
-                                                        'datasets' => [
-                                                            [
-                                                                'backgroundColor' => "rgba(179,181,198,0.2)",
-                                                                'borderColor' => "rgba(179,181,198,1)",
-                                                                'pointBackgroundColor' => "rgba(179,181,198,1)",
-                                                                'pointBorderColor' => "#fff",
-                                                                'pointHoverBackgroundColor' => "#fff",
-                                                                'pointHoverBorderColor' => "rgba(179,181,198,1)",
-                                                                'data' => $prices,
-                                                                'fill' => false,
-                                                                'stepped' => true
-                                                            ],
-
-                                                        ]
-                                                    ]
-                                                ]);
-                                                ?>
-                                            </td>
-
-                                        </tr>
-                                        <?php $dates = [];
-                                        $prices = []; ?>
-                                    <?php endif; ?>
-                                    <tr class="spacer"></tr>
-                                <?php endif; ?>
-
                             <?php endforeach; ?>
                             </tbody>
                         <?php else: ?>
