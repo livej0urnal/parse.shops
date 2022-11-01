cca09f8b0914166a4581e93197082f9717ba6738
Igor Filippov <anubis3009@gmail.com>
2022-11-01 19:55:19 +0200

    #changes_part2_all_in_one-table

--- controllers/ProductsController.php
+++ controllers/ProductsController.php
@@ -3,8 +3,8 @@
 namespace app\controllers;
 
 use app\controllers\AppController;
-use app\models\LeaProducts;
 use app\models\Products;
+use app\models\ZenithProducts;
 use Yii;
 
 class ProductsController extends AppController
@@ -12,12 +12,12 @@ class ProductsController extends AppController
     public function actionChange()
     {
         $id = Yii::$app->request->get('id');
-        $changes = LeaProducts::find()->all();
+        $changes = ZenithProducts::find()->all();
         foreach ($changes as $change)
         {
             $product = new Products();
             $product->attributes = $change->attributes;
-            $product->seller = 'Lea';
+            $product->seller = 'Zenith';
             $product->save();
             echo $product->id . '<br>';
             $change->delete();
