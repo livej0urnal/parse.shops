a338a9033a6a67b2e5d6b519d0de5f7ae477db99
Igor Filippov <anubis3009@gmail.com>
2022-11-02 16:31:23 +0200

    #changes_part2_optimization_last

--- controllers/EicController.php
+++ controllers/EicController.php
@@ -2,12 +2,12 @@
 
 namespace app\controllers;
 
+use app\models\Products;
+use app\models\Updates;
 use Yii;
 use yii\data\Sort;
 use yii\web\Controller;
 use app\models\Eic;
-use app\models\EicProducts;
-use app\models\EicUpdates;
 use yii\db\Expression;
 use yii\data\Pagination;
 
@@ -19,25 +19,13 @@ class EicController extends AppController
         foreach ($links as $link) {
             $link->delete();
         }
-        $products = EicProducts::find()->all();
+        $products = Products::find()->where(['seller' => 'EIC'])->indexBy('seller')->all();
         foreach ($products as $product)
         {
             $product->instock = null;
             $product->save(false);
         }
         return $this->render('links');
-    }
-
-    public function actionImages()
-    {
-        $products = EicProducts::find()->all();
-        foreach ($products as $product)
-        {
-            if($product->image != '/Content/Images/NoImage.png') {
-                $product->image = '/uploads/eic/' . $product->sku . '.png';
-                $product->save(false);
-            }
-        }
     }
 
     public function actionParse()
@@ -57,34 +45,32 @@ class EicController extends AppController
                 $product->sku = preg_replace("/[^0-9]/", '', $product->sku);
                 $product->price = trim($product->find('span.price1', 0)->plaintext);
                 $product->price = preg_replace("/[^,.0-9]/", '', $product->price);
-                $find_product = EicProducts::findOne(['sku' => $product->sku]);
+                $find_product = Products::findOne(['sku' => $product->sku]);
                 if(!empty($find_product)) {
-                    $need_update = EicUpdates::find()->where(['sku_product' => $product->sku])->orderBy(['id' => SORT_DESC])->one();
-                    if(!$need_update->price) {
-                        $product_update = EicProducts::findOne(['sku' => $product->sku]);
+                    $need_update = Updates::find()->where(['sku_product' => $product->sku])->orderBy(['id' => SORT_DESC])->one();
+                    if($need_update->price != $product->price) {
+                        $product_update = Products::findOne(['sku' => $product->sku]);
                         $product_update->price = $product->price;
+                        if(!empty($product->find('div.description', 1)->plaintext)) {
+                            $product_update->per = $product->find('div.description', 1)->plaintext;
+                        }
                         $product_update->instock = '1';
-                        $product_update->seller = 'EIC';
+                        $product->updated_at = new Expression('NOW()');
                         $product_update->save(false);
+                        $new_updates = new Updates();
+                        $new_updates->price = $product->price;
+                        $new_updates->sku_product = $product->sku;
+                        $new_updates->update_at = $product->updated_at;
+                        $new_updates->save(false);
                     }
                     else{
-                        $new_updates = new EicUpdates();
-                        $new_updates->sku_product = htmlspecialchars($product->sku);
-                        $new_updates->price = htmlspecialchars($product->price);
-                        $product_update = EicProducts::findOne(['sku' => $product->sku]);
-                        $product_update->price = $product->price;
-                        $product_update->updated_at = new Expression('NOW()');
-                        $product_update->instock = '1';
-                        $product_update->seller = 'EIC';
-                        $product_update->save(false);
-                        $new_updates->save(false);
-
-                        $update_products++;
+                        $find_product->instock = '1';
+                        $find_product->save(false);
                     }
 
                 }
                 else{
-                    $new_product = new EicProducts();
+                    $new_product = new Products();
                     $new_product->sku = $product->sku;
                     $product->image = $product->find('img.catalog-img ', 0)->getAttribute('src');
                     $product->title = $product->find('div.product-title' , 0)->plaintext;
@@ -107,83 +93,11 @@ class EicController extends AppController
                     $new_product->instock = '1';
                     $new_product->seller = 'EIC';
                     $new_product->save(false);
-
-                    $new_updates = new EicUpdates();
-                    $new_updates->sku_product = htmlspecialchars($product->sku);
-                    $new_updates->price = htmlspecialchars($product->price);
-                    $new_updates->save(false);
-
-                    $new_products ++ ;
                 }
                 $parse_products ++ ;
 
             }
         }
         return $this->render('parse', compact('links', 'parse_products', 'update_products' , 'new_products'));
-    }
-
-    public function actionIndex()
-    {
-        $id = Yii::$app->request->get('id');
-        $products = EicProducts::find()->orderBy(['id' => SORT_DESC])->limit(10)->all();
-        $sort = new Sort([
-            'attributes' => [
-                'updated_at',
-                'price',
-                'instock',
-            ],
-            'defaultOrder' => ['updated_at' => SORT_DESC]
-        ]);
-        $query = EicProducts::find()->indexBy('sku')->orderBy($sort->orders);
-        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 500, 'forcePageParam' => false, 'pageSizeParam' => false]);
-        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
-        $manufactures = EicProducts::find()->select('article')->orderBy(['article' => SORT_DESC])->groupBy(['article'])->all();
-        $this->setMeta('European Importing Company');
-        return $this->render('index' , compact('products', 'pages', 'manufactures', 'sort'));
-    }
-
-    public function actionSearch($q)
-    {
-        $q = Yii::$app->request->get('q');
-        $words = explode(' ',  $q);
-        foreach ($words as $word) {
-            $products = EicProducts::find()->where(['like', 'title', $word])->orWhere(['like', 'sku' , $word])->orderBy(['id' => SORT_DESC])->all();
-            $sort = new Sort([
-                'attributes' => [
-                    'updated_at',
-                    'price',
-                    'instock',
-                ],
-                'defaultOrder' => ['updated_at' => SORT_DESC]
-            ]);
-            $query = EicProducts::find()->where(['like', 'title', $word])->orWhere(['like', 'sku' , $word])->orWhere(['like', 'article' , $word])->orderBy($sort->orders);
-            $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 50, 'forcePageParam' => false, 'pageSizeParam' => false]);
-            $products = $query->offset($pages->offset)->limit($pages->limit)->all();
-        }
-
-
-        $manufactures = EicProducts::find()->select('article')->orderBy(['article' => SORT_DESC])->groupBy(['article'])->all();
-        $this->setMeta('European Importing Company');
-        return $this->render('index' , compact('products', 'pages', 'q', 'manufactures', 'sort'));
-    }
-
-    public function actionManufacture($q)
-    {
-        $q = Yii::$app->request->get('q');
-        $products = EicProducts::find()->where(['like', 'article', $q])->orderBy(['id' => SORT_DESC])->all();
-        $sort = new Sort([
-            'attributes' => [
-                'updated_at',
-                'price',
-                'instock',
-            ],
-            'defaultOrder' => ['updated_at' => SORT_DESC]
-        ]);
-        $query = EicProducts::find()->where(['like', 'article', $q])->orderBy($sort->orders);
-        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 50, 'forcePageParam' => false, 'pageSizeParam' => false]);
-        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
-        $manufactures = EicProducts::find()->select('article')->orderBy(['article' => SORT_DESC])->groupBy(['article'])->all();
-        $this->setMeta('European Importing Company');
-        return $this->render('index' , compact('products', 'pages', 'q', 'manufactures', 'sort'));
     }
 }
