4dfe0b187e71f2808f746af0199597cc0550495b
Igor Filippov <anubis3009@gmail.com>
2022-11-02 16:35:14 +0200

    #changes_part2_optimization_last

--- controllers/EuphoriaController.php
+++ controllers/EuphoriaController.php
@@ -2,12 +2,12 @@
 
 namespace app\controllers;
 
+use app\models\Products;
+use app\models\Updates;
 use Yii;
 use yii\data\Sort;
 use yii\web\Controller;
 use app\models\Euphoria;
-use app\models\EuphoriaProducts;
-use app\models\EuphoriaUpdates;
 use yii\db\Expression;
 use yii\data\Pagination;
 use app\controllers\AppController;
@@ -20,7 +20,7 @@ class EuphoriaController extends AppController
         foreach ($links as $link) {
             $link->delete();
         }
-        $products = EuphoriaProducts::find()->all();
+        $products = Products::find()->where(['seller' => 'Euphoria'])->indexBy('seller')->all();
         foreach ($products as $product) {
             $product->instock = null;
             $product->save(false);
@@ -44,32 +44,32 @@ class EuphoriaController extends AppController
                 $product->sku = preg_replace("/[^0-9]/", '', $product->sku);
                 $product->price = trim($product->find('span.price1', 0)->plaintext);
                 $product->price = preg_replace("/[^,.0-9]/", '', $product->price);
-                $find_product = EuphoriaProducts::findOne(['sku' => $product->sku]);
-                if (!empty($find_product)) {
-                    $need_update = EuphoriaUpdates::find()->where(['sku_product' => $product->sku])->orderBy(['id' => SORT_DESC])->one();
-                    if ($need_update->price) {
-                        $product_update = EuphoriaProducts::findOne(['sku' => $product->sku]);
+                $find_product = Products::findOne(['sku' => $product->sku]);
+                if(!empty($find_product)) {
+                    $need_update = Updates::find()->where(['sku_product' => $product->sku])->orderBy(['id' => SORT_DESC])->one();
+                    if($need_update->price != $product->price) {
+                        $product_update = Products::findOne(['sku' => $product->sku]);
                         $product_update->price = $product->price;
+                        if(!empty($product->find('div.description', 1)->plaintext)) {
+                            $product_update->per = $product->find('div.description', 1)->plaintext;
+                        }
                         $product_update->instock = '1';
-                        $product_update->seller = 'Euphoria';
+                        $product->updated_at = new Expression('NOW()');
                         $product_update->save(false);
-                    } else {
-                        $new_updates = new EuphoriaUpdates();
-                        $new_updates->sku_product = htmlspecialchars($product->sku);
-                        $new_updates->price = htmlspecialchars($product->price);
-                        $product_update = EuphoriaProducts::findOne(['sku' => $product->sku]);
-                        $product_update->price = $product->price;
-                        $product_update->updated_at = new Expression('NOW()');
-                        $product_update->instock = '1';
-                        $product_update->seller = 'Euphoria';
-                        $product_update->save(false);
+                        $new_updates = new Updates();
+                        $new_updates->price = $product->price;
+                        $new_updates->sku_product = $product->sku;
+                        $new_updates->update_at = $product->updated_at;
                         $new_updates->save(false);
-
-                        $update_products++;
+                    }
+                    else{
+                        $find_product->instock = '1';
+                        $find_product->save(false);
                     }
 
-                } else {
-                    $new_product = new EuphoriaProducts();
+                }
+                else {
+                    $new_product = new Products();
                     $new_product->sku = $product->sku;
                     $product->image = $product->find('img.catalog-img ', 0)->getAttribute('src');
                     $product->title = $product->find('div.product-title', 0)->plaintext;
@@ -91,78 +91,11 @@ class EuphoriaController extends AppController
                     $new_product->instock = '1';
                     $new_product->seller = 'Euphoria';
                     $new_product->save(false);
-
-                    $new_updates = new EuphoriaUpdates();
-                    $new_updates->sku_product = htmlspecialchars($product->sku);
-                    $new_updates->price = htmlspecialchars($product->price);
-                    $new_updates->save(false);
-
-                    $new_products++;
                 }
                 $parse_products++;
 
             }
         }
         return $this->render('parse', compact('links', 'parse_products', 'update_products', 'new_products'));
-    }
-
-    public function actionIndex()
-    {
-        $id = Yii::$app->request->get('id');
-        $products = EuphoriaProducts::find()->orderBy(['id' => SORT_DESC])->limit(10)->all();
-        $sort = new Sort([
-            'attributes' => [
-                'updated_at',
-                'price',
-                'instock',
-            ],
-            'defaultOrder' => ['updated_at' => SORT_DESC]
-        ]);
-        $query = EuphoriaProducts::find()->indexBy('sku')->orderBy($sort->orders);
-        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 500, 'forcePageParam' => false, 'pageSizeParam' => false]);
-        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
-        $manufactures = EuphoriaProducts::find()->select('article')->orderBy(['article' => SORT_DESC])->groupBy(['article'])->all();
-        $this->setMeta('Euphoria Fancy Food LC');
-        return $this->render('index', compact('products', 'pages', 'manufactures', 'sort'));
-    }
-
-    public function actionSearch($q)
-    {
-        $q = Yii::$app->request->get('q');
-        $products = EuphoriaProducts::find()->where(['like', 'title', $q])->orWhere(['like', 'sku', $q])->orderBy(['id' => SORT_DESC])->all();
-        $sort = new Sort([
-            'attributes' => [
-                'updated_at',
-                'price',
-                'instock',
-            ],
-            'defaultOrder' => ['updated_at' => SORT_DESC]
-        ]);
-        $query = EuphoriaProducts::find()->where(['like', 'title', $q])->orWhere(['like', 'sku' , $q])->orWhere(['like', 'article' , $q])->orderBy($sort->orders);
-        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 50, 'forcePageParam' => false, 'pageSizeParam' => false]);
-        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
-        $manufactures = EuphoriaProducts::find()->select('article')->orderBy(['article' => SORT_DESC])->groupBy(['article'])->all();
-        $this->setMeta('Euphoria Fancy Food LC');
-        return $this->render('index', compact('products', 'pages', 'q', 'manufactures', 'sort'));
-    }
-
-    public function actionManufacture($q)
-    {
-        $q = Yii::$app->request->get('q');
-        $products = EuphoriaProducts::find()->where(['like', 'article', $q])->orderBy(['id' => SORT_DESC])->all();
-        $sort = new Sort([
-            'attributes' => [
-                'updated_at',
-                'price',
-                'instock',
-            ],
-            'defaultOrder' => ['updated_at' => SORT_DESC]
-        ]);
-        $query = EuphoriaProducts::find()->where(['like', 'article', $q])->orderBy($sort->orders);
-        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 50, 'forcePageParam' => false, 'pageSizeParam' => false]);
-        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
-        $manufactures = EuphoriaProducts::find()->select('article')->orderBy(['article' => SORT_DESC])->groupBy(['article'])->all();
-        $this->setMeta('Euphoria Fancy Food LC');
-        return $this->render('index', compact('products', 'pages', 'q', 'manufactures', 'sort'));
     }
 }
