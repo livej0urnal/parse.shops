431f22d89aaaf1e381e6eabbdea18bde62171553
Igor Filippov <anubis3009@gmail.com>
2022-11-03 14:35:24 +0200

    #optimization_view

--- controllers/UpdatesController.php
+++ controllers/UpdatesController.php
@@ -15,7 +15,7 @@ class UpdatesController extends AppController
 
             'cache' => [
                 'class' => 'yii\filters\PageCache',
-                'only' => [''],
+                'only' => ['index'],
                 'duration' => 36000,
             ],
         ];
@@ -26,7 +26,7 @@ class UpdatesController extends AppController
         $id = Yii::$app->request->get('id');
         ini_set('max_execution_time', 900);
         $products = Products::getDb()->cache(function (){
-            return Products::find()->with('updates', 'last')->indexby('id')->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
+            return Products::find()->indexby('id')->with('updates', 'last')->orderBy(['updated_at' => SORT_DESC])->andWhere(('updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)'))->all();
         }, 3600);
         $this->setMeta('Last Updates from 7 Days');
         return $this->render('index', compact('products'));
